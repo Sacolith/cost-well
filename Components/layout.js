a8e5cd10d6af9c8287bc997cb5c62bd1b26d4be5
Sacolith <69515678+Sacolith@users.noreply.github.com>
2022-08-04 19:20:37 -0500

    more layout stuff

--- Components/layout.js
+++ Components/layout.js
@@ -9,7 +9,7 @@ export default function Layout({ title, children }) {
         <meta name="description" content="Online Shopping" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
+      <div className="flex min-h-screen flex-col justify-between">
         <header>header</header>
         <main>{children}</main>
         <footer>footer </footer>
