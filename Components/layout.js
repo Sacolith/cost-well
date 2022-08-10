38e6010f7e43d80d1f8a8ee1ad295b80874bfb53
Sacolith <69515678+Sacolith@users.noreply.github.com>
2022-08-09 20:51:57 -0500

    dispatch error add to cart errot

--- Components/layout.js
+++ Components/layout.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Link from 'next/link';
 import Head from 'next/head';
+import { Store } from '../utils/Store';
 
 export default function Layout({ title, children }) {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <>
       <Head>
-        <title>{title ? title + '-Valtemina Group' : 'Cost Well'}</title>
+        <title>
+          {title ? title + '-Valtemina Group-Costwell' : 'Cost Well'}
+        </title>
         <meta name="description" content="Online Shopping" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex min-h-screen flex-col justify-between">
-        <header>header</header>
-        <main>{children}</main>
-        <footer>footer </footer>
-      </div>
+      <>
+        <div>
+          <header className="flex-auto flex-col justify-between">
+            <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+              <Link href="/">
+                <a className="text-lg font-bold">Costwell</a>
+              </Link>
+              <div>
+                <Link href="/cart">
+                  <a className="p-2">
+                    Cart
+                    {cart.cartItems.length > 0 && (
+                      <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                        {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                      </span>
+                    )}
+                  </a>
+                </Link>
+                <Link href="/login">
+                  <a className="p-2">Login</a>
+                </Link>
+              </div>
+            </nav>
+          </header>
+          <main className="container m-auto mt-4 px-4 flex-auto min-h-screen flex-col justify-between">
+            {children}
+          </main>
+          <footer className="shadow-inner flex h-10 justify-center items-center min-h-screen flex-col ">
+            <p>Copyright @ 2022 Valtemina Group-CostWell</p>
+          </footer>
+        </div>
+      </>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 import Layout from '../Components/layout';
+import ProductItem from '../Components/ProductItem';
+import data from '../utils/data';
 
 export default function Home() {
-  return <Layout title="Home Page">Home page</Layout>;
+  return (
+    <Layout title="Home Page">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
+    </Layout>
+  );
 }
